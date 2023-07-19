d4dddb1fdb4e93f602dc77e33c8a28be1956a204
MrBear-xzx <1276063856@qq.com>
2023-07-20 01:02:54 +0800

    Mybatis内容补充

--- Mybatis/src/main/java/mapper/BlogMapper.java
+++ Mybatis/src/main/java/mapper/BlogMapper.java
@@ -4,6 +4,7 @@ import main.java.entity.Blog;
 
 public interface BlogMapper {
 
+    // 多参数时会封装成一个map，为了不混乱，可以用@Param来指定封装时使用的key
 //    @Select("SELECT * FROM blog WHERE id = #{id}")
     Blog selectBlog(Integer id);
 }
