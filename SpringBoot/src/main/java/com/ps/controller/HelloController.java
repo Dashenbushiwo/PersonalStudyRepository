5460e6e9b41acb8633451996ca3ae3103bfbcc6e
MrBear-xzx <1276063856@qq.com>
2023-08-18 00:38:50 +0800

    SpringBoot

--- SpringBoot/src/main/java/com/ps/controller/HelloController.java
+++ SpringBoot/src/main/java/com/ps/controller/HelloController.java
@@ -2,14 +2,27 @@ package com.ps.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
+@RequestMapping("/hello/")
 @Controller
-@RestController("/hello/")
+//@RestController("/hello/")
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("hello")
     public String hello() {
         return "Hello SpringBoot";
     }
+
+    @RequestMapping("helloThymeleaf")
+    public String helloThymeleaf(Map<String, Object> map) {
+        // 只要把页面放在classpath:/templates/下，thymeleaf就会自动渲染
+        // thymeleaf- classpath:/templates/index.html
+        map.put("hello", "你好，thymeleaf");
+        return "index";
+    }
+
 }
--- SpringBoot/src/test/java/com/ps/ApplicationTests.java
+++ SpringBoot/src/test/java/com/ps/ApplicationTests.java
@@ -1,11 +1,13 @@
 package com.ps;
 
 import com.ps.bean.Person;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootTest
 class ApplicationTests {
 
@@ -16,12 +18,17 @@ class ApplicationTests {
     private ApplicationContext ioc;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         // 1. 测试@PropertySource和@ConfigurationProperties
         System.out.println(person);
         // 2. 测试@ImportResource
         Object dog = ioc.getBean("dog");
         System.out.println(dog);
+    }
+
+    @Test
+    public void testLogs() {
+        log.debug("打个日志玩一玩");
     }
 
 }
